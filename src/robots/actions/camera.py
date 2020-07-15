37a2f0d6d615b5fac2bbdf34fa4c89d0148fb1ed
Daniel Rodriguez <danielrs9705@gmail.com>
2020-07-15 12:04:24 +0200

    Reduce the time to take the picture

--- src/robots/actions/camera.py
+++ src/robots/actions/camera.py
@@ -30,5 +30,5 @@ Input
 '''
 def take_picture(camera_topic):
     camera_topic.subscribe(receive_image)
-    time.sleep(1)
-    camera_topic.unsubscribe()
+    time.sleep(0.15)
+    camera_topic.unsubscribe() # This avoid to take many photos after one movement
