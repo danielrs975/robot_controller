05212220859b94d7b6c96b0d46941a8a15922d81
Daniel Rodriguez <danielrs9705@gmail.com>
2020-07-15 11:59:15 +0200

    Add code to control the camera

--- src/robots/giraff.py
+++ src/robots/giraff.py
@@ -7,6 +7,8 @@ import roslibpy
 import time
 
 from robots.actions.move import execute_move
+from robots.actions.camera import take_picture
+
 '''
 Class that represents the robot Giraff. This is the version 1.
 '''
@@ -29,12 +31,14 @@ class Giraff():
     '''
     def __init__(self, client):
         self.move_topic = roslibpy.Topic(client, '/cmd_vel', 'geometry_msgs/Twist')
+        self.camera_topic = roslibpy.Topic(client, '/camera1/image_raw/compressed', 'sensor_msgs/CompressedImage')
 
     '''
     Move the robot in the given direction
     '''
     def move_robot(self, move):
         execute_move(move, self.move_topic)
+        take_picture(self.camera_topic)
 
     '''
     Remove the listeners to the topics of the robot
