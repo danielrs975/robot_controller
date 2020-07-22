50312e6c04219ca12994396bae0505792e3368ae
Daniel Rodriguez <danielrs9705@gmail.com>
2020-07-22 12:47:40 +0200

    Add some comments to the giraff robot

--- src/robots/giraff.py
+++ src/robots/giraff.py
@@ -1,7 +1,7 @@
 '''
 These script contains several functions to move the robot
 around
-NOTE: Needs to be adapted after to the physical robot way to move
+NOTE: Needs to be adapted after to the physical robot way to move (In process)
 '''
 import roslibpy
 import time
@@ -9,26 +9,27 @@ import time
 from robots.actions.move import execute_move
 from robots.actions.camera import take_picture
 
-'''
-Class that represents the robot Giraff. This is the version 1.
-'''
+
 class Giraff():
     '''
-    Constructor of Giraff
+    Class that represents the robot Giraff. This is the version 1.
     '''
     def __init__(self, client):
+        '''
+        Constructor of Giraff
+        '''
         self.move_topic = roslibpy.Topic(client, '/cmd_vel', 'geometry_msgs/Twist')
-        self.camera_topic = roslibpy.Topic(client, '/camera1/image_raw/compressed', 'sensor_msgs/CompressedImage')
+        self.camera_topic = roslibpy.Topic(client, '/teresa_robot/head_camera/image_raw/compressed', 'sensor_msgs/CompressedImage')
 
-    '''
-    Move the robot in the given direction
-    '''
     def move_robot(self, move):
+        '''
+        Move the robot in the given direction
+        '''
         execute_move(move, self.move_topic)
         take_picture(self.camera_topic)
 
-    '''
-    Remove the listeners to the topics of the robot
-    '''
     def remove_subscribers(self):
+        '''
+        Remove the listeners to the topics of the robot
+        '''
         self.move_topic.unadvertise()
