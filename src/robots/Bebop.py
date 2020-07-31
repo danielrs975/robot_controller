662c97880befc179a472c4312945e6a691317f22
Daniel Rodriguez <danielrs9705@gmail.com>
2020-07-31 17:01:35 +0200

    Create first version of the Bebop python controller

---
+++ src/robots/Bebop.py
@@ -0,0 +1,78 @@
+"""
+Contains the necessary code to control bebop 2
+"""
+from robots.Robot import Robot
+from robots.actions.move import execute_move
+from robots.actions.camera import take_picture
+
+# To control the bebop simulation the following topic is used
+#   /bebop/command/trajectory
+
+def create_msg(position):
+    return {
+        'points': [
+            {
+                'transforms': [
+                    {
+                        'translation': position
+                    }
+                ]
+            }
+        ]
+    }
+
+class Bebop(Robot):
+    """
+    Class that represents the Bebop 2 Power Drone
+    """
+    STEP_FOR_MOVEMENT = 0.20
+    POSSIBLE_MOVES = []
+
+    POSITION = {
+        'x': 0.0,
+        'y': 0.0,
+        'z': 0.0
+    }
+
+    MOVE_TOPIC = {
+        'topic_name': '/bebop/command/trajectory',
+        'msg_type': 'trajectory_msgs/MultiDOFJointTrajectory'
+    }
+    NUMBER_MOVEMENTS = 4
+
+    def __init__(self, client):
+        """
+        Constructor of the class
+        """
+        super(Bebop, self).__init__(client, '/bebop/camera1', self.MOVE_TOPIC, self.POSSIBLE_MOVES)
+
+    def takeoff(self):
+        """
+        With this we can takeoff the drone
+        """
+        self.POSITION['z'] = 1.0
+        execute_move(create_msg(self.POSITION), self.move_topic)
+
+    def move_robot(self, move):
+        if move == 0:
+            # Move left
+            self.POSITION['y'] += self.STEP_FOR_MOVEMENT
+        elif move == 1:
+            # Move right
+            self.POSITION['y'] -= self.STEP_FOR_MOVEMENT
+        elif move == 2:
+            # Move foward
+            self.POSITION['x'] -= self.STEP_FOR_MOVEMENT
+        elif move == 3:
+            # Move backward
+            self.POSITION['x'] += self.STEP_FOR_MOVEMENT
+
+        execute_move(create_msg(self.POSITION), self.move_topic)
+        take_picture(self.camera_topic)
+
+    def land(self):
+        """
+        Land the dron in the ground
+        """
+        self.POSITION['z'] = 0
+        execute_move(create_msg(self.POSITION), self.move_topic)
