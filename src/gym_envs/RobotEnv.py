c7a924b0d3415dd63e06cd38e25792ea67148500
Daniel Rodriguez <danielrs9705@gmail.com>
2020-08-17 11:45:30 +0200

    Add feature to the gym env

--- src/gym_envs/RobotEnv.py
+++ src/gym_envs/RobotEnv.py
@@ -39,8 +39,25 @@ class RobotEnv(gym.Env):
     def render(self, mode='human'):
         image = cv2.imread('./env_observation', 0)
         window_name = 'image'
+        x, y = 0, 0 # Replace this by the actual square position
+
+        start_point = (x, y)
+
+        end_point = (x + 32, y + 32)
+
+        color = (255, 0, 0)
+
+        thickness = 2
+        
+        image = cv2.rectangle(image, start_point, end_point, color, thickness)
 
         cv2.imshow(window_name, image)
+        value = 5
+        imgcpy = image.copy()
+
+        img = cv2.resize(imgcpy, None, fx=0.5, fy=0.5)
+        cv2.imshow(window_name, image)
+        
         cv2.waitKey(1)
         # cv2.destroyAllWindows()
         return 0
